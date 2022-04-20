538464850406932c89ba0b4ab2aaa320a6f3825c
asehgal <sehgal.abhi@gmail.com>
2022-04-20 17:02:16 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/K8sDemoAppApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import com.example.demo.config.MyConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableConfigurationProperties(MyConfig.class)
+@EnableScheduling
+public class K8sDemoAppApplication {
+
+	@Value("${welcome.message}")
+	private String message;
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sDemoAppApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World " + message;
+	}
+}
